dc2f14d99f610df0f374e8ab3b101edc56c4625e
dsv01.trevilub@gmail.com <aa678990>
2017-09-06 11:30:15 -0300

    First commit

--- angularjs/routes.js
+++ angularjs/routes.js
@@ -7,10 +7,22 @@ angular.module('app').config([
 
         $stateProvider
 
-            // LOGIN
-            .state('/', {
-                url: '/',
-            })
+        // MAIN
+        .state('/', {
+            url: '/',
+            templateUrl: './angularjs/views/main.html'
+        })
+
+        // FORM
+        .state('add', {
+            url: '/add',
+            templateUrl: './angularjs/views/form.html'
+        })
+
+        .state('edit', {
+            url: '/edit/:id',
+            templateUrl: './angularjs/views/form.html'
+        })
 
         $urlRouterProvider.otherwise('/');
 
